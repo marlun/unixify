290c955014d6e578ea6c4a83e9c340a10ac9cc96
Martin Lundberg <martin.lundberg@axis.com>
2017-12-27 23:09:48 +0100

    Re-render on 'render' event

--- index.js
+++ index.js
@@ -5,13 +5,25 @@ const morph = require('nanomorph')
 const nanorouter = require('nanorouter')
 const nanobus = require('nanobus')
 
+const bus = nanobus()
+const events = { RENDER: 'render' }
 const router = nanorouter()
 let state = { msg: 'hello, world' }
 let tree = null
 
+bus.prependListener(events.RENDER, function () {
+  var newTree = router(window.location.pathname)
+  morph(tree, newTree)
+})
+
 router.on('/', function () {
   return html`<body><div>${state.msg}</div></body>`
 })
+
+setTimeout(function () {
+  state.msg = 'awesome'
+  bus.emit('render')
+}, 2000)
 
 documentReady(function () {
   tree = document.querySelector('body')
